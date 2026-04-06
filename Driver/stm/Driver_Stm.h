6032219fbe2cace833b3b7202ba65d266fb0c9d7
rgtuu <hs011010@naver.com>
2026-04-07 00:03:27 +0900

    test: Core를 사용한 testbench 생성

--- Driver/stm/Driver_Stm.h
+++ Driver/stm/Driver_Stm.h
@@ -55,8 +55,8 @@ typedef struct
 /*********************************************************************************************************************/
 /*------------------------------------------------Function Prototypes------------------------------------------------*/
 /*********************************************************************************************************************/
-extern SchedulingFlag st_scheduling_info;
-extern stTestCnt st_test_cnt_info;
+extern volatile SchedulingFlag st_scheduling_info;
+extern volatile stTestCnt st_test_cnt_info;
 
 void Driver_Stm_Init(void);
 
--- TriCore Debug (TASKING)/Driver/can/subdir.mk
+++ TriCore Debug (TASKING)/Driver/can/subdir.mk
@@ -18,7 +18,7 @@ OBJS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 "Driver/can/Driver_Can.src":"../Driver/can/Driver_Can.c" "Driver/can/subdir.mk"
-	cctc -cs --dep-file="$*.d" --misrac-version=2012 -D__CPU__=tc27xd "-fC:/Users/USER/AURIX-v1.10.28-workspace/TC275_ACT_ECU/TriCore Debug (TASKING)/TASKING_C_C___Compiler-Include_paths__-I_.opt" --iso=99 --c++14 --language=+volatile --exceptions --anachronisms --fp-model=3 -O0 --tradeoff=4 --compact-max-size=200 -g -Wc-w544 -Wc-w557 -Ctc27xd -Y0 -N0 -Z0 -o "$@" "$<"
+	cctc -cs --dep-file="$*.d" --misrac-version=2012 -D__CPU__=tc27xd "-fC:/Users/USER/AURIX-v1.10.28-workspace/TC275_ACT_tb/TriCore Debug (TASKING)/TASKING_C_C___Compiler-Include_paths__-I_.opt" --iso=99 --c++14 --language=+volatile --exceptions --anachronisms --fp-model=3 -O0 --tradeoff=4 --compact-max-size=200 -g -Wc-w544 -Wc-w557 -Ctc27xd -Y0 -N0 -Z0 -o "$@" "$<"
 "Driver/can/Driver_Can.o":"Driver/can/Driver_Can.src" "Driver/can/subdir.mk"
 	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
 
